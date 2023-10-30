e1a52f2e1643addb6a59ba3ac11722d9e0b72f9d
fooweiguo <68820158+fooweiguo@users.noreply.github.com>
2023-10-30 21:52:36 +0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,55 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+# Uncomment the region, access_key and secret_key if you are running locally
+provider "aws" {
+   region = "us-east-1"
+   access_key = "ACCESS_KEY" # Update accordingly
+   secret_key = "SECRET_KEY" # Update accordingly
+}
+
+data "aws_iam_policy_document" "queue" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions   = ["sqs:SendMessage"]
+    resources = ["arn:aws:sqs:*:*:fooweiguo-s3-event-notification-queue"]
+
+    condition {
+      test     = "ArnEquals"
+      variable = "aws:SourceArn"
+      values   = [aws_s3_bucket.bucket.arn]
+    }
+  }
+}
+
+resource "aws_sqs_queue" "queue" {
+  name   = "fooweiguo-s3-event-notification-queue"
+  policy = data.aws_iam_policy_document.queue.json
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "fooweiguo-s3-sqs"
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.bucket.id
+
+  queue {
+    queue_arn     = aws_sqs_queue.queue.arn
+    events        = ["s3:ObjectCreated:*"]
+    filter_suffix = ".log"
+  }
+}
