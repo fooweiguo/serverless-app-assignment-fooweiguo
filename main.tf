a295af1065a981b4e625acd887ec2294f082c0fe
fooweiguo <68820158+fooweiguo@users.noreply.github.com>
2023-10-30 21:53:59 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
 # Configure the AWS Provider
 # Uncomment the region, access_key and secret_key if you are running locally
 provider "aws" {
